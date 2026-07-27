3b8efd57f6ed1217c2bdb583603e5840d3528040
lightman210567 <81330314+lightman210567@users.noreply.github.com>
2026-07-27 17:33:49 +0100

    270726/4
    Signed-off-by: lightman210567 <81330314+lightman210567@users.noreply.github.com>

--- src/main/java/com/lightman210567/testingmod/ContainerBlock.java
+++ src/main/java/com/lightman210567/testingmod/ContainerBlock.java
@@ -2,12 +2,21 @@ package com.lightman210567.testingmod;
 
 import com.mojang.serialization.MapCodec;
 import net.minecraft.core.BlockPos;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.ItemInteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.BaseEntityBlock;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.EntityBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.BlockHitResult;
 
 public class ContainerBlock extends Block implements EntityBlock {
     public ContainerBlock (BlockBehaviour.Properties properties) {
@@ -17,5 +26,22 @@ public class ContainerBlock extends Block implements EntityBlock {
     @Override
     public BlockEntity newBlockEntity(BlockPos blockPos, BlockState blockState) {
         return new ContainerEntity(blockPos, blockState);
+    }
+
+    @Override
+    public ItemInteractionResult useItemOn(ItemStack stack, BlockState state, Level level,
+                                           BlockPos pos, Player player, InteractionHand hand,
+                                           BlockHitResult hitResult) {
+        if (level.getBlockEntity(pos) instanceof ContainerEntity containerEntity) {
+            if (containerEntity.inventory.getStackInSlot(0).isEmpty() && !stack.isEmpty()) {
+                containerEntity.inventory.insertItem(0, stack.copy(), false);
+                stack.shrink(1);
+                level.playSound(player, pos, SoundEvents.AMETHYST_CLUSTER_BREAK, SoundSource.BLOCKS, 1.0F, 1.0F);
+            } else if (stack.isEmpty()) {
+                ItemStack containerStack = containerEntity.inventory.extractItem(0, 1, false);
+                player.setItemInHand(InteractionHand.MAIN_HAND, containerStack);
+            }
+        }
+        return ItemInteractionResult.SUCCESS;
     }
 }
--- src/main/java/com/lightman210567/testingmod/ContainerEntity.java
+++ src/main/java/com/lightman210567/testingmod/ContainerEntity.java
@@ -1,48 +1,75 @@
 package com.lightman210567.testingmod;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.HolderLookup;
 import net.minecraft.core.NonNullList;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
+import net.minecraft.world.Containers;
+import net.minecraft.world.SimpleContainer;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.entity.BaseContainerBlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.neoforged.neoforge.items.ItemStackHandler;
 
 import static com.lightman210567.testingmod.TestingMod.CONTAINER_ENTITY;
 
-public class ContainerEntity extends BaseContainerBlockEntity {
-    public static final int SIZE = 10;
+public class ContainerEntity extends BlockEntity {
+    public static final int SLOT_COUNT = 1;
+    public static final int STACK_SIZE = 64;
 
-    private NonNullList<ItemStack> items = NonNullList.withSize(SIZE, ItemStack.EMPTY);
+    public final ItemStackHandler inventory = new ItemStackHandler(SLOT_COUNT) {
+      @Override
+      protected int getStackLimit(int slot, ItemStack stack) {
+        return STACK_SIZE;
+      }
+
+      @Override
+      protected void onContentsChanged(int slot) {
+        setChanged();
+        if (!level.isClientSide) {
+            level.sendBlockUpdated(getBlockPos(), getBlockState(), getBlockState(), 3);
+        }
+      }
+    };
+
+    // not required
+    public void clearContents() {
+        for (int i = 0; i < SLOT_COUNT; i++) {
+            inventory.setStackInSlot(i, ItemStack.EMPTY);
+        }
+    }
+
+    public void drops() {
+        SimpleContainer inv = new SimpleContainer(inventory.getSlots());
+        for (int i = 0; i < inventory.getSlots(); i++) {
+            inv.setItem(i, inventory.getStackInSlot(i));
+        }
+
+        Containers.dropContents(this.level, this.worldPosition, inv);
+    }
+
+    @Override
+    protected void saveAdditional(CompoundTag tag, HolderLookup.Provider registries) {
+        super.saveAdditional(tag, registries);
+        tag.put("inventory", inventory.serializeNBT(registries));
+    }
+
+    @Override
+    protected void loadAdditional(CompoundTag tag, HolderLookup.Provider registries) {
+        super.loadAdditional(tag, registries);
+        inventory.deserializeNBT(registries, tag.getCompound("inventory"));
+    }
 
     public ContainerEntity(BlockPos pos, BlockState state) {
         super(CONTAINER_ENTITY.get(), pos, state);
     }
 
-    @Override
-    public int getContainerSize() {
-        return SIZE;
-    }
-
-    @Override
-    protected NonNullList<ItemStack> getItems() {
-        return items;
-    }
-
-    @Override
-    protected void setItems(NonNullList<ItemStack> items) {
-        this.items = items;
-    }
-
-    @Override
-    protected Component getDefaultName() {
-        return Component.translatable("container.testingmod.container_entity");
-    }
-
-    @Override
-    protected AbstractContainerMenu createMenu(int containerId, Inventory inventory) {
-        return null;
-    }
+    // TODO: IMPLEMENT SYNC ON THE CONTAINER ENTITY
+    // there is a bug where you will be able to insert an item to a full inventory
+    // after you have reloaded the save, due to syncing isses
+    // see https://docs.neoforged.net/docs/1.21.1/blockentities/#syncing-on-block-update
 }
